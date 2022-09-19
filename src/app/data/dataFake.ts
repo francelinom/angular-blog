0eac7921c793325734103f067744f3a9ffc642b3
Francelino <fmarcilio1989@gmail.com>
2022-09-19 11:27:33 -0300

    adicionando banco de fake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    id: 1,
+    photo: 'aaa',
+    title: 'Saiu novo filme',
+    description: 'Olá',
+  },
+  {
+    id: 2,
+    photo: 'bbb',
+    title: 'Saiu novo filme 2',
+    description: 'Olá 2',
+  },
+  {
+    id: 3,
+    photo: 'ccc',
+    title: 'Saiu novo filme 3',
+    description: 'Olá 3',
+  },
+];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
+  photoCover: string =
+    'https://observatoriodocinema.uol.com.br/wp-content/uploads/2022/08/O-Incrivel-Hulk-Divulgacao-1200x900-1.jpg';
+  contentTitle: string = 'Título';
+  contentDescription: string = 'fdfsfsdfdfsfsd';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
